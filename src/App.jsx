24a2101e174f28cd4cad7f3e91a802977989fbb4
kartikpavan <kartik.varanasi@yahoo.com>
2022-11-01 18:09:20 +0530

    mobile responsive hero section

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,12 @@ const App = () => {
 		<div className="flex w-screen h-screen min-h-screen flex-col items-center justify-center relative ">
 			{/* NAVBAR */}
 			<Navbar />
-			<div className="h-screen grid grid-cols-2 items-center">
+			{/* <div className="w-screen md:w-10/12 h-screen flex flex-col-reverse md:flex-row justify-between items-center">
 				<div className="w-full h-96  bg-bodybg text-textBase ">
+					<Hero />
+				</div> */}
+			<div className="flex w-screen h-screen min-h-screen md:w-10/12 flex-col-reverse md:flex-row justify-center items-center relative">
+				<div className="w-full bg-bodybg text-textBase p-4">
 					<Hero />
 				</div>
 				<div className="w-full h-full">
@@ -17,9 +21,11 @@ const App = () => {
 				</div>
 			</div>
 
-			<div className="absolute bottom-10 w-full flex justify-center items-center">
+			{/* </div> */}
+
+			<div className="absolute bottom-72 md:bottom-10 w-full flex justify-center items-center">
 				<div className="rounded-2xl shadow-md p-4 flex items-center justify-center bg-zinc-800">
-					<p className="text-slate-300">Press and Drag to Orbit</p>
+					<p className="text-slate-300 text-sm md:text-md">Press and Drag to Orbit</p>
 				</div>
 			</div>
 		</div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Hero = () => {
 	return (
-		<div className="w-[30rem] mx-auto bg-navbarBg rounded-2xl shadow-sm shadow-purple-300 px-5 py-5 text-lg">
+		<div className=" w-full md:w-[30rem] mx-auto bg-navbarBg rounded-2xl shadow-sm shadow-purple-300 px-5 py-5 text-lg">
 			<h1 className="leading-loose">Hi, My name is</h1>
 			<h1 className="text-5xl text-slate-200 leading-relaxed">Divya Kartik Pavan</h1>
 			<h1 className="font-extrabold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
