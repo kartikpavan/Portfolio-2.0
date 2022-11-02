542f2ea718c76d93208ea23afc39b1a36d744a02
kartikpavan <kartik.varanasi@yahoo.com>
2022-11-02 20:03:53 +0530

    Added tooltop on image hover

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -14,20 +14,20 @@ import tailwind from "../assets/skills/tailwind.png";
 import sass from "../assets/skills/sass.png";
 
 const dataSource = [
-	{ src: html, alt: "html" },
-	{ src: css, alt: "css" },
-	{ src: js, alt: "js" },
-	{ src: ts, alt: "typescript" },
-	{ src: react, alt: "react" },
-	{ src: node, alt: "nodejs" },
-	{ src: tailwind, alt: "tailwind" },
-	{ src: redux, alt: "redux" },
-	{ src: mongo, alt: "mongo" },
-	{ src: sql, alt: "sql" },
-	{ src: firebase, alt: "firebase" },
-	{ src: github, alt: "github" },
-	{ src: express, alt: "express" },
-	{ src: sass, alt: "sass" },
+	{ src: html, alt: "html", name: "HTML" },
+	{ src: css, alt: "css", name: "CSS" },
+	{ src: js, alt: "js", name: "JAVASCRIPT" },
+	{ src: ts, alt: "typescript", name: "TYPESCRIPT" },
+	{ src: react, alt: "react", name: "REACT JS" },
+	{ src: node, alt: "nodejs", name: "NODE JS" },
+	{ src: tailwind, alt: "tailwind", name: "TAILWIND CSS" },
+	{ src: redux, alt: "redux", name: "REDUX" },
+	{ src: mongo, alt: "mongo", name: "MONGO DB" },
+	{ src: sql, alt: "sql", name: "MY SQL" },
+	{ src: firebase, alt: "firebase", name: "FIREBASE" },
+	{ src: github, alt: "github", name: "GITHUB" },
+	{ src: express, alt: "express", name: "EXPRESS JS" },
+	{ src: sass, alt: "sass", name: "SASS" },
 ];
 
 const Skills = () => {
@@ -39,15 +39,20 @@ const Skills = () => {
 			</h2>
 			<div className="my-24">
 				<div className="flex flex-wrap gap-8 justify-center md:max-w-2xl item-center max-w-xs">
-					{dataSource.map(({ alt, src }) => (
-						<img
-							key={alt}
-							className="object-contain fitler grayscale hover:grayscale-0 transition-all ease-out"
-							src={src}
-							width={60}
-							height={60}
-							alt={alt}
-						/>
+					{dataSource.map(({ alt, src, name }) => (
+						<div className="group">
+							<img
+								key={alt}
+								className="object-contain fitler grayscale hover:grayscale-0 transition-all ease-out"
+								src={src}
+								width={60}
+								height={60}
+								alt={alt}
+							/>
+							<span class='tooltip-text bg-purple-300 p-3 -mt-24 -ml-6 rounded hidden group-hover:block absolute text-center py-2 px-6 z-50"'>
+								{name}
+							</span>
+						</div>
 					))}
 				</div>
 			</div>
